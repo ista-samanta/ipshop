17c2cf76cdb3e83d7ffd140726829942bb8e55e4
ista-samanta <ista.samanta@gmail.com>
2016-12-16 10:17:03 +0100

    Update entrypoint.sh
    Regex modified

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-IP=$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)
-IP6=$(ip addr show dev 'eth0' | sed -e's/^.*inet6 \([^ ]*\)\/.*$/\1/;t;d')
-sed -e 's/IP_CONTAINER/'"$IP"'/g' sed -e 's/IP6_CONTAINER/'"$IP6"'/g' /tmp/template-index.html > /tmp/template2-index.html
-#sed -e 's/IP6_CONTAINER/'"$IP6"'/g' /tmp/template-index.html > /tmp/template2-index.html
-sed -e 's/HOSTNAME_CONTAINER/'"$HOSTNAME"'/g' /tmp/template2-index.html > /usr/local/apache2/htdocs/index.html
-echo "ServerName www.unicen-example.com:80" >> /usr/local/apache2/conf/httpd.conf 
+IP=$(ip addr show dev 'eth0' | grep ^[[:blank:]]*inet|grep global|sed -e 's@/@ @g'|awk '{print $2}')
+IP6=$(ip addr show dev 'eth0' | grep ^[[:blank:]]*inet6|grep global|sed -e 's@/@ @g'|awk '{print $2}')
+FileName=/tmp/template-index.html
+sed -e 's/IP_CONTAINER/'"$IP"'/g' -e 's/IP6_CONTAINER/'"$IP6"'/g' -i $FileName
+sed -e 's/HOSTNAME_CONTAINER/'"$HOSTNAME"'/g' $FileName > /usr/local/apache2/htdocs/index.html
+echo "ServerName www.unicen-example.com:80" >> /usr/local/apache2/conf/httpd.conf
 echo "#######CONTAINER IP#######"$IP"#######"
 echo "#######CONTAINER IPv6#######"$IP6"#######"
 httpd -D FOREGROUND
