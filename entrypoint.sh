06b96fd5e3b4538025f15fc70d47d2d0a212a514
ista-samanta <ista.samanta@gmail.com>
2016-12-16 11:12:55 +0100

    Update entrypoint.sh
    Global filter added

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-IP=$(ip addr show dev 'eth0' | grep ^[[:blank:]]*inet|grep global|sed -e 's@/@ @g'|awk '{print $2}')|grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b'
-IP6=$(ip addr show dev 'eth0' | grep ^[[:blank:]]*inet6|grep global|sed -e 's@/@ @g'|awk '{print $2}')
-FileName=/tmp/template-index.html
-sed -e 's/IP_CONTAINER/'"$IP"'/g' -e 's/IP6_CONTAINER/'"$IP6"'/g' -i $FileName
-sed -e 's/HOSTNAME_CONTAINER/'"$HOSTNAME"'/g' $FileName > /usr/local/apache2/htdocs/index.html
+set -x
+IP=$(ip addr show dev 'eth0' | sed -e'0,/inet/ s/^.*inet \([^ ]*\)\/.*$/\1/;t;d')
+IP6=$(ip addr show dev 'eth0' | sed -e'0,/inet6/ s/^.*inet6 \([^ ]*\)\/.*$/\1/;t;d')
+sed -e 's/IP_CONTAINER/'"$IP"'/g' sed -e 's/IP6_CONTAINER/'"$IP6"'/g' /tmp/template-index.html > /tmp/template2-index.html
+sed -e 's/HOSTNAME_CONTAINER/'"$HOSTNAME"'/g' /tmp/template2-index.html > /usr/local/apache2/htdocs/index.html
 echo "ServerName www.unicen-example.com:80" >> /usr/local/apache2/conf/httpd.conf
 echo "#######CONTAINER IP#######"$IP"#######"
 echo "#######CONTAINER IPv6#######"$IP6"#######"
